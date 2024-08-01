64b545ffae0504f071c203772e36ff3d45e99596
Sergey Ardintsev <archunter13@yandex.ru>
2024-08-01 22:02:33 +0300

    dnd edited

--- todo-list.js
+++ todo-list.js
@@ -76,44 +76,12 @@ const todo = {
   todoList.addEventListener(`dragend`, (evt) => {
     evt.target.classList.remove(`selected`);
   });
-
-  // todoList.addEventListener(`dragover`, (evt) => {
-  //   // Разрешаем сбрасывать элементы в эту область
-  //   evt.preventDefault();
   
-  //   // Находим перемещаемый элемент
-  //   const activeElement = todoList.querySelector(`.selected`);
-  //   // Находим элемент, над которым в данный момент находится курсор
-  //   const currentElement = evt.target;
-  //   // Проверяем, что событие сработало:
-  //   // 1. не на том элементе, который мы перемещаем,
-  //   // 2. именно на элементе списка
-  //   const isMoveable = activeElement !== currentElement &&
-  //     currentElement.classList.contains(`todo__item`);
-  
-  //   // Если нет, прерываем выполнение функции
-  //   if (!isMoveable) {
-  //     return;
-  //   }
-  
-  //   // Находим элемент, перед которым будем вставлять
-  //   const nextElement = (currentElement === activeElement.nextElementSibling) ?
-  //       currentElement.nextElementSibling :
-  //       currentElement;
-  
-  //   // Вставляем activeElement перед nextElement
-  //   todoList.insertBefore(activeElement, nextElement);
-  // });
-
-
-const getNextElement = (cursorPosition, currentElement) => {
-  // Получаем объект с размерами и координатами
+const getNextElement = (cursorPosition, currentElement) => { 
   const currentElementCoord = currentElement.getBoundingClientRect();
-  // Находим вертикальную координату центра текущего элемента
+  
   const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
-
-  // Если курсор выше центра элемента, возвращаем текущий элемент
-  // В ином случае — следующий DOM-элемент
+  
   const nextElement = (cursorPosition < currentElementCenter) ?
       currentElement :
       currentElement.nextElementSibling;
@@ -132,19 +100,15 @@ todoList.addEventListener(`dragover`, (evt) => {
     if (!isMoveable) {
       return;
     }
+      const nextElement = getNextElement(evt.clientY, currentElement);
   
-    // evt.clientY — вертикальная координата курсора в момент,
-    // когда сработало событие
-    const nextElement = getNextElement(evt.clientY, currentElement);
-  
-    // Проверяем, нужно ли менять элементы местами
+    
     if (
       nextElement &&
       activeElement === nextElement.previousElementSibling ||
       activeElement === nextElement
     ) {
-      // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
-      return;
+    return
     }
   
     todoList.insertBefore(activeElement, nextElement);
